ed3c040b0f486fca7de7d434d78d419f3ff139f6
root <root@ik1-433-48940.vs.sakura.ne.jp>
2024-07-20 10:28:17 +0900

    ひな形作成

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>イシュー管理システム</title>
+</head>
+<body>
+    
+</body>
+</html>
